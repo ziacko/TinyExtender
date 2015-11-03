2caded9abda3ad315020123a85b54a2cff1176eb
ziyad barakat <ziyadbarakat@gmail.com>
2015-11-04 03:40:26 +1100

    fixed a bunch of missing/broken opengl extensions. also updated copy of tinywindow that comes with the example.

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -2,19 +2,21 @@
 #include "TinyWindow.h"
 int main()
 {
-	WindowManager::Initialize();
-	WindowManager::AddWindow("TinyExtenderTest");
+	windowManager* Manager = new windowManager();
+
+	Manager->Initialize();
+	Manager->AddWindow("TinyExtender Test");
 
 	TinyExtender::InitializeExtensions();
 
 	GLuint Program = glCreateProgram();
 
-	while(!WindowManager::GetWindowShouldClose("TinyExtenderTest"))
+	while(!Manager->GetWindowShouldCloseByIndex(0))
 	{
-		WindowManager::PollForEvents();
+		Manager->PollForEvents();
 		glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		WindowManager::WindowSwapBuffers("TinyExtenderTest");
+		Manager->WindowSwapBuffersByIndex(0);
 	}
 
 	return 0;
